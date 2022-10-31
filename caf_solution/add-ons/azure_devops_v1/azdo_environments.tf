34501aa17b06f341bddbeac91275a3f11a7378ed
malikwaqas124 <115136130+malikwaqas124@users.noreply.github.com>
2022-10-31 11:14:22 +0800

    Create azdo_environments.tf

---
+++ caf_solution/add-ons/azure_devops_v1/azdo_environments.tf
@@ -0,0 +1,6 @@
+resource "azuredevops_environment" "environment" {
+  for_each = var.environments
+  project_id   = data.azuredevops_project.project[each.value.project_key].id
+  name         = each.value.name
+  description  = try(each.value.description, null)
+}
